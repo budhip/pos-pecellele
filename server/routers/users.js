fd98b551f4553265be0d75e262c7a45cc923208b
Adith <adithwidyapradipta@gmail.com>
2017-08-04 16:29:35 +0700

    fitur edit dan edit di modal menu dan user

--- server/routers/users.js
+++ server/routers/users.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const userCont = require('../controllers/userController');
 const userAuth = require('../helpers/verifyToken');
 
-router.get('/', userAuth.verifyTokenForAdmin, userCont.getAllUsers);
-router.get('/:id', userAuth.verifyTokenForAdminKasir, userCont.getOneUser);
-router.post('/', userAuth.verifyTokenForAdmin, userCont.createNewUser);
-router.put('/:id', userAuth.verifyTokenForAdminKasir, userCont.updateUser);
-router.delete('/:id', userAuth.verifyTokenForAdmin, userCont.deleteUser);
+router.get('/', userCont.getAllUsers);
+router.get('/:id', userCont.getOneUser);
+router.post('/', userCont.createNewUser);
+router.put('/:id', userCont.updateUser);
+router.delete('/:id', userCont.deleteUser);
 
 module.exports = router
