8c173d367aa4f5e7256c33c25969a093a5b97ce3
jumadiakhmad <jumadiakhmad2@gmail.com>
2017-08-04 10:54:49 +0700

    login

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const logger = require('morgan')
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://mongobudhi:ILoveYou123@cluster0-shard-00-00-frloc.mongodb.net:27017,cluster0-shard-00-01-frloc.mongodb.net:27017,cluster0-shard-00-02-frloc.mongodb.net:27017/dbpecellelepos?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
 
 let app = express();
 
 app.use(cors());
+app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
